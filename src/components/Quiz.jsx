7403d0b97a903f9de8de36a3135f15a595a2671c
vighnesh-kargutkar <kargutkar860@gmail.com>
2024-11-16 00:07:16 +0530

    key added for questionTimer component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -32,7 +32,11 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
-        <QuestionTimer timeout={10000} onTimeout={handleSelectAnswer} />
+        <QuestionTimer
+          timeout={10000}
+          onTimeout={handleSelectAnswer}
+          key={activeQuestionIndex}
+        />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
